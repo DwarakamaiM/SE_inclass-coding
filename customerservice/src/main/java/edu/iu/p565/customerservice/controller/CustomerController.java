2b3bb03b9c0450771951ae005510e24167113fc6
Dwarakamai Mannemuddu <dwarakamaimannemuddu@Dwarakamais-MacBook-Pro.local>
2023-03-23 13:41:42 -0400

    added customer

--- customerservice/src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
+++ customerservice/src/main/java/edu/iu/p565/customerservice/controller/CustomerController.java
@@ -1,13 +1,20 @@
 package edu.iu.p565.customerservice.controller;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.iu.p565.customerservice.model.Customer;
+
+
 @RestController
-@RequestMapping
-public class CustomerServiceController {
+@RequestMapping("/customers")
+public class CustomerController {
+    
     @GetMapping
-	public String greetings() {
-		return "Greetings!";
-	}
+    public List<Customer> findall(){
+        return null;
+    }
 }
